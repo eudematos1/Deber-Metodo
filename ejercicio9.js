36cb9c2ffb820b907569c89277bc7c885d6ced9d
eudematos <eudematos@sudamericano.edu.ec>
2023-01-27 10:21:33 -0500

    second commit

--- ejercicio9.js
+++ ejercicio9.js
@@ -1,11 +1,13 @@
 let nums = [0, 0, 0, 0]
 let x=prompt("Ingrese numero")
-let j=nums.length-1
+let j=3
 let i=0
+
 
 for(let i=0;i<nums.length;i++){
     nums[i]=x
     
     x=x*2
 }
-console.log(nums)
+
+console.log(nums[j]+" ," +nums[j-1]+" ,"+nums[j-2]+" ,"+nums[j-3])
